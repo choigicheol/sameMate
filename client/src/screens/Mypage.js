a3cccf6e5466a884a5431b08745b91a363fe56ce
choigicheol <kofpo900916@gmail.com>
2022-09-10 19:55:25 +0900

    [Mypage] menuBar 수정

--- client/src/screens/Mypage.js
+++ client/src/screens/Mypage.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { data } from "../../dummy/data";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { data, menus } from "../../dummy/data";
+import MenuTap from "../components/MenuTap";
 
-export default function Mypage() {
+export default function Mypage({ navigation }) {
   const favoriteNum = data[0].favorite.length;
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>좋아하는 영상 수 {favoriteNum}</Text>
+    <>
+      <View style={styles.container}>
+        <ScrollView
+          horizontal={false}
+          contentContainerStyle={styles.resultContainer}
+        >
+          <View>
+            <Text>좋아하는 영상 수 {favoriteNum}</Text>
+          </View>
+        </ScrollView>
+        <View style={styles.MenuContainer}>
+          {menus.map((menu) => (
+            <MenuTap key={menu.id} title={menu.title} navigation={navigation} />
+          ))}
+        </View>
       </View>
-      <View style={styles.MenuContainer}>
-        {menus.map((menu) => (
-          <MenuTap key={menu.id} title={menu.title} navigation={navigation} />
-        ))}
-      </View>
-    </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#fff" },
+  MenuContainer: {
+    flexDirection: "row",
+    backgroundColor: "#32AAFF",
+  },
+});

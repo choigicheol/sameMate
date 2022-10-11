23d769dd11f37bf34dc3463300133a91e503ad40
choigicheol <kofpo900916@gmail.com>
2022-10-11 18:03:42 +0900

    redux 수정, 로그아웃 시 리셋

--- client/src/redux/slice/movieSlice.js
+++ client/src/redux/slice/movieSlice.js
@@ -2,12 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   movie_list: [],
+  sameUserData: [],
 };
 
 export const movieSlice = createSlice({
   name: "movie",
   initialState,
   reducers: {
+    setMovie: (state, action) => {
+      state.movie_list = [...action.payload];
+    },
     addMovie: (state, action) => {
       state.movie_list = [...action.payload, ...state.movie_list];
     },
@@ -16,8 +20,21 @@ export const movieSlice = createSlice({
         ...state.movie_list.filter((movie) => movie.id != action.payload),
       ];
     },
+    setSameUserData: (state, action) => {
+      state.sameUserData = [...action.payload];
+    },
+    movieSliceReset: (state) => {
+      state.movie_list = [];
+      state.sameUserData = [];
+    },
   },
 });
 
-export const { addMovie, deleteMovie } = movieSlice.actions;
+export const {
+  setMovie,
+  addMovie,
+  deleteMovie,
+  setSameUserData,
+  movieSliceReset,
+} = movieSlice.actions;
 export default movieSlice.reducer;
--- client/src/redux/slice/userSlice.js
+++ client/src/redux/slice/userSlice.js
@@ -1,18 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  uid: "",
   name: "",
   email: "",
   accessToken: "",
+  sameUserList: [],
+  isLogin: false,
 };
+
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
     setUser(state, action) {
+      state.uid = action.payload.uid;
       state.email = action.payload.email;
       state.name = action.payload.name;
       state.accessToken = action.payload.accessToken;
+      state.isLogin = action.payload.isLogin;
     },
     setName(state, action) {
       state.name = action.payload;
@@ -23,10 +29,28 @@ const userSlice = createSlice({
     setAccessToken(state, action) {
       state.accessToken = action.payload;
     },
+    setSameUserList(state, action) {
+      state.sameUserList = [...action.payload];
+    },
+    userSliceReset(state) {
+      state.uid = "";
+      state.email = "";
+      state.name = "";
+      state.accessToken = "";
+      state.sameUserList = [];
+      state.isLogin = false;
+    },
   },
   //extraReducer는 비동기 액션 생성시 필요
   // extraReducers: builder => {},
 });
 
-export const { setUser, setName, setEmail, setAccessToken } = userSlice.actions;
+export const {
+  setUser,
+  setName,
+  setEmail,
+  setAccessToken,
+  setSameUserList,
+  userSliceReset,
+} = userSlice.actions;
 export default userSlice.reducer;

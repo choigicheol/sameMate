872287a9f0ad4697cf64f78f4a43706179d611ba
choigicheol <kofpo900916@gmail.com>
2022-10-17 15:54:13 +0900

    font 설정(적용x)

--- client/App.js
+++ client/App.js
@@ -1,15 +1,32 @@
+import React, { useEffect, useState, useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import Navigation from "./src/navigation/Navigation";
 import { statusBarHeight } from "./src/util/WH";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./src/redux/store/store";
 import { Provider } from "react-redux";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+// import "react-native-gesture-handler";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    oneMobile: require("./src/assets/fonts/ONEMobileBold.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }, [fontsLoaded]);
+
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
